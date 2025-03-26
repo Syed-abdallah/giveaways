80a11ba6d223a61c4186682819280ae9cb1db3eb
Syed Abdullah <myselfsyedabdullah@gmail.com>
2025-03-26 15:59:25 +0500

    link

--- routes/auth.php
+++ routes/auth.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register')
-                ->middleware(['auth', 'verified']);
+                ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
