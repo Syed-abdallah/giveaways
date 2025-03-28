c9bd4ce53687abdfcf65642ec42aed67594875f4
Syed Abdullah <myselfsyedabdullah@gmail.com>
2025-03-29 00:36:22 +0500

    location

--- app/Http/Middleware/CountryRestrictionMiddleware.php
+++ app/Http/Middleware/CountryRestrictionMiddleware.php
@@ -9,21 +9,16 @@ class CountryRestrictionMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        // Get the user's public IP
         $userIp = $request->header('X-Forwarded-For') ?? $request->ip();
-        
-        // Use an external API to get country data
+
         $geoData = json_decode(file_get_contents("http://ip-api.com/json/{$userIp}"), true);
 
-        // Check if the API call was successful
         if ($geoData && isset($geoData['countryCode'])) {
-            $userCountry = $geoData['countryCode']; // e.g., "PK" for Pakistan
+            $userCountry = $geoData['countryCode'];
 
-            // List of blocked countries (modify as needed)
-            $blockedCountries = ['PK']; // Add more if needed
+            $blockedCountries = ['PK'];
 
-            // Allowed users (specific IPs)
-            $allowedIps = []; // Add your own IP to bypass restriction
+            $allowedIps = [];
 
             if (in_array($userCountry, $blockedCountries) && !in_array($userIp, $allowedIps)) {
                 // return response()->json([
